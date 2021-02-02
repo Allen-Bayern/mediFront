06f02559534de80e3eb934b73882652b4cdce446
ZZY <allenz@ZZYdeMacBook-Pro.local>
2021-02-02 11:24:24 +0800

    reset the vuetify

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { appOptions, head }) {
   head.link.push({
     rel: 'stylesheet',
     href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
