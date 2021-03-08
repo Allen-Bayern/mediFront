334dcef2ddc40ffb6043426a4d9b26ed7bc7515a
yuebing <zhaozhengyang_byn@pku.edu.cn>
2021-03-08 17:56:53 +0800

    nothing

--- karma.conf.js
+++ karma.conf.js
@@ -25,13 +25,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/**/*.test.js': ['coverage']
     },
+
+    coverageReporter: {
+      type: 'html',  //可以用其他的比如text
+      dir:  'coverage/'
+    }, //配置覆盖率报告的查看方式
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', `coverage`],
 
 
     // web server port
