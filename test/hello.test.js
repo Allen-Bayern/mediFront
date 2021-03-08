a865e13d51ce4112e6adff25016de341447c72dc
yuebing <zhaozhengyang_byn@pku.edu.cn>
2021-03-09 02:19:58 +0800

    ddd

--- test/hello.test.js
+++
@@ -1,8 +0,0 @@
-import Vue from `vue`;
-import hello from `@/page/Hello`
-
-describe(`test hello world`, () => {
-    it('Is hello world shown?', () => {
-        expect('hello world').toEqual(hello());
-    })
-})
